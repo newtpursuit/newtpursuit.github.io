2541b017917056bee4160d2daf426910dbe1ff25
newtpursuit <135772865+newtpursuit@users.noreply.github.com>
2023-06-26 19:48:33 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,7 +47,7 @@ function draw() {
     textSize(32);
     contents = "Congratulations, you've completed my game!\nI had fun; I hope you feel the same\n\nHere's a reward:";
     text(contents, width/2, height/4);
-    link = createA("javascript:alert('Sorry, I'm not quite ready yet. Check in again soon :)');", "Download (might take a minute)");
+    link = createA("javascript:alert('Sorry, I\'m not quite ready yet. Check in again soon :)'", "Download (might take a minute)");
     // link.attribute("download", "funny.mp4");
     link.center();
     pop();
